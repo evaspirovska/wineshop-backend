de4bc02b2493bd578e28e1a22d81adae691ca143
GP <markmark204@gmail.com>
2021-10-06 21:00:58 +0200

    initial commit

---
+++ src/main/java/com/systems/integrated/wineshopbackend/WineshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.systems.integrated.wineshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineshopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/systems/integrated/wineshopbackend/WineshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systems.integrated.wineshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
