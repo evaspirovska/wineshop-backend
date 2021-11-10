7e34439b8f9a7b93d281e4120412ce93939c8cf4
evaspirovska <67806019+evaspirovska@users.noreply.github.com>
2021-11-11 00:55:21 +0100

    order changes

--- src/main/java/com/systems/integrated/wineshopbackend/models/orders/DTO/ProductInOrderDTO.java
+++ src/main/java/com/systems/integrated/wineshopbackend/models/orders/DTO/ProductInOrderDTO.java
@@ -3,10 +3,10 @@ package com.systems.integrated.wineshopbackend.models.orders.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class ProductInOrderDTO {
-
-    private Long productId;
-    private int quantity;
-}
+//@AllArgsConstructor
+//@Getter
+//public class ProductInOrderDTO {
+//
+//    private Long productId;
+//    private int quantity;
+//}
